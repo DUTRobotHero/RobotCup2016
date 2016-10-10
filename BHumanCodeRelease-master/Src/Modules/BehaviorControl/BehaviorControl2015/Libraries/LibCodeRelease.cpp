b8219c637810466c70f48ddff0c2fcac5d5c442d
Williams1996 <2015287834@qq.com>
2016-10-10 15:51:44 +0800

    Format LibCoderelease

--- BHumanCodeRelease-master/Src/Modules/BehaviorControl/BehaviorControl2015/Libraries/LibCodeRelease.cpp
+++ BHumanCodeRelease-master/Src/Modules/BehaviorControl/BehaviorControl2015/Libraries/LibCodeRelease.cpp
@@ -21,20 +21,28 @@ namespace Behavior2015
 
 	      std::vector<Obstacle> p = theObstacleModel.obstacles;
 	      std::vector<float> angles;
-	      if ( p.size () == 0 ) {
-		  y = 0.0;
-	      } else if ( p.size () == 1 ) {
-		  y = p[0].left.y () + 50.f;
-	      } else if ( p.size () > 1 ) {
-		  for ( unsigned int i = 0; i < p.size () - 1; i++ ) {
-		      angles.push_back ( LibCodeRelease::getAngle ( p[i].right, p[i + 1].left ) );
-		  }
-		  for ( unsigned int k = 0; k < angles.size (); k++ ) {
+	      if ( p.size () == 0 ) 
+            {
+            y = 0.0;
+            } 
+          else if ( p.size () == 1 ) 
+            {
+            y = p[0].left.y () + 50.f;
+            } 
+          else if ( p.size () > 1 ) 
+            {
+                for ( unsigned int i = 0; i < p.size () - 1; i++ ) 
+                {
+                    angles.push_back ( LibCodeRelease::getAngle ( p[i].right, p[i + 1].left ) );
+                }
+            for ( unsigned int k = 0; k < angles.size (); k++ )
+            {
 		      float temp = 0.0;
 		      int numOfObstacel = 0;
-		      if ( angles[k] > temp ) {
-			  temp = angles[k];
-			  numOfObstacel = k;
+		      if ( angles[k] > temp ) 
+              {
+                  temp = angles[k];
+                  numOfObstacel = k;
 		      }
 		      y = ( p[numOfObstacel].left.y () + p[numOfObstacel].right.y () ) / 2.000;
 		  }
@@ -43,12 +51,15 @@ namespace Behavior2015
 	      angleToGoal
 	          = ( theRobotPose.inverse () * Vector2f ( theFieldDimensions.xPosOpponentGroundline, y ) ).angle ();
 	      printf ( "the info of Goal-after: %f\n", angleToGoal * 180 / 3.1415926 );
-	  } else {
+        } 
+    
+        else 
+        {
 	      y = 0.0f;
 	      angleToGoal
 	          = ( theRobotPose.inverse () * Vector2f ( theFieldDimensions.xPosOpponentGroundline, y ) ).angle ();
 	      printf ( "the info of Goal-before: %f\n", angleToGoal * 180 / 3.1515926 );
-	  }
+        }
       }
       else
 	  angleToGoal= ( theRobotPose.inverse () * Vector2f ( theFieldDimensions.xPosOpponentGroundline, 0.0f ) ).angle ();
