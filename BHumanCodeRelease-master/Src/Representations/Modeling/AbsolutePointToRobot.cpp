cea54164b2550209c5a34d029fddbb74a665ab9f
17skye17 <841577908@qq.com>
2016-10-12 21:14:30 +0800

    updated ready state,first turn and then walk straight

--- BHumanCodeRelease-master/Src/Representations/Modeling/AbsolutePointToRobot.cpp
+++ BHumanCodeRelease-master/Src/Representations/Modeling/AbsolutePointToRobot.cpp
@@ -8,24 +8,11 @@
 
 Pose2f AbsolutePointToRobot(const RobotPose& theRobotPose,float Absolute_X,float Absolute_Y)
 {
-    /* float a = Absolute_X - theRobotPose.translation.x();
-     float b = Absolute_Y - theRobotPose.translation.y();
-     float temprot = std::atan(b / a);
-     float rot2robot = temprot - theRobotPose.rotation;
-     Vector2f temp(a,b);
-     float c = temp.norm();
-     float Relate_X = c * std::cos(rot2robot);
-     float Relate_Y = c * std::sin(rot2robot);
-     Vector2f relate(Relate_X,Relate_Y);
-    Pose2f RobotPosition(rot2robot,relate);
-    return RobotPosition;*/
-    
-  //  x=x'cost-y'sint+x0,  y=x'sint+y'cost+y0.
      float a = Absolute_X - theRobotPose.translation.x();
      float b = Absolute_Y - theRobotPose.translation.y();
      float Relate_X = a * std::cos(theRobotPose.rotation) + b * std::sin(theRobotPose.rotation);
      float Relate_Y = b * std::cos(theRobotPose.rotation) - a * std::sin(theRobotPose.rotation);
-     float rot2robot = 0.0;
+     float rot2robot = std::atan(Relate_Y/Relate_X);
      Vector2f relate(Relate_X,Relate_Y);
      Pose2f RobotPosition(rot2robot,relate);
      return RobotPosition;
