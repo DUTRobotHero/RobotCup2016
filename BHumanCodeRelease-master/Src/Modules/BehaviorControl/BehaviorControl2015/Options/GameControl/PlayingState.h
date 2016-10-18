dc8b4a38f7f9f5bedafe1463dbc6f6912b2fe95d
17skye17 <841577908@qq.com>
2016-10-18 16:30:12 +0800

    add 2 defenders and exchanged the ready places of 3 and 5

--- BHumanCodeRelease-master/Src/Modules/BehaviorControl/BehaviorControl2015/Options/GameControl/PlayingState.h
+++ BHumanCodeRelease-master/Src/Modules/BehaviorControl/BehaviorControl2015/Options/GameControl/PlayingState.h
@@ -1,11 +1,77 @@
 option(PlayingState)
 {
-       initial_state(start)
-       {
-           action
-           {
-                    theHeadControlMode = HeadControl::lookForward;
-                    Stand();
-           }
-       }
+
+
+    int KEEPER_NUMBER = 1;
+    int STRIKER_NUMBER = 2;
+    int SUPPORTER_NUMBER = 3;
+	int DEFENDER_NUMBER1 = 4;
+    int DEFENDER_NUMBER2 = 5;
+	initial_state(upstand)
+  {
+    transition
+    {
+      if(theFallDownState.state == FallDownState::upright && action_done)
+        goto play;
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::lookForward;
+      Stand();
+    }
+  }
+  state(play)
+  {
+    transition
+    {
+        if (state_time>5000){
+			goto changeRoles;
+		}
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::leftAndRight;
+    }
+  }
+
+  state(changeRoles)
+  {
+      transition
+      {
+          if (libCodeRelease.returnFormPenalty == true ){
+			  libCodeRelease.returnFormPenalty=false;
+              OUTPUT_TEXT("libCodeRelease.returnFormPenalty" << libCodeRelease.returnFormPenalty);
+              OUTPUT_TEXT("theOwnSideModel.returnFromGameControllerPenalty" << theOwnSideModel.returnFromGameControllerPenalty);
+              OUTPUT_TEXT("theOwnSideModel.returnFromManualPenalty" << theOwnSideModel.returnFromManualPenalty);
+			  goto backToField;
+			  }
+      }
+      action
+      {
+          ShowBool(theOwnSideModel.returnFromGameControllerPenalty);
+          ShowBool(theOwnSideModel.returnFromManualPenalty);
+          ChooseRoles();
+      }
+  }
+  state(backToField)
+  {
+      transition
+      {
+		  if ( theRobotInfo.number == KEEPER_NUMBER ){
+				Pose2f relatePoint=AbsolutePointToRobot(theRobotPose,theFieldDimensions.xPosOwnPenaltyMark,0);
+				if (relatePoint.translation.norm() < 100.f)
+					goto changeRoles;
+		   }else{
+			   Pose2f relatePoint=AbsolutePointToRobot(theRobotPose,theFieldDimensions.xPosOwnPenaltyMark,0);
+				if (relatePoint.translation.norm() < 100.f)
+					goto changeRoles;
+		   }
+           if(action_done)
+               goto play;
+      }
+      action
+      {
+          ReadyState();
+      }
+  }
 }
