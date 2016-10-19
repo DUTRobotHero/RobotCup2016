3801847228e06da73b4f2459f0835355181e9fea
HaxiSnake <haxisnake@sina.com>
2016-10-19 23:49:59 +0800

    hope this work the supporter

--- BHumanCodeRelease-master/Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/Supporter.h
+++ BHumanCodeRelease-master/Src/Modules/BehaviorControl/BehaviorControl2015/Options/Roles/Supporter.h
@@ -1,4 +1,4 @@
-option(helpStriker)
+option(Supporter)
 {
 	float setAdjustVelocity = 1.f;
 	float setTurnVelocity = pi/8;
@@ -7,18 +7,25 @@ option(helpStriker)
 		transition{
 			goto sideright;
 		}
+		action{
+      theHeadControlMode = HeadControl::lookForward;
+    }
 	}
-	//当strikerd的位置大于50cm，supporter去右边
-	state(sideleft)
+		state(sideleft)
 	{
 		transition{
 			if(otherTeammate.pose.translation.y()>=1000.f)
 					goto sideright;
+			if(libCodeRelease.timeSinceBallWasSeen() > 10000)
+					goto searchForBall;
+			if(libCodeRelease.timeSinceBallWasSeen() < 300 && theRobotPose.translation.x()>2000.f)
+				goto turnToBall;
 		}
 		action{
+			theHeadControlMode = HeadControl::searchForBall;
 			Pose2f relatePoint = AbsolutePointToRobot(theRobotPose,3200.f,1100.f);
 			WalkToTarget(Pose2f( setTurnVelocity,setAdjustVelocity,setAdjustVelocity),
-                    Pose2f(theBallModel.estimate.position.angle(),relatePoint.translation.x(),relatePoint.translation.y()));
+                    Pose2f(libCodeRelease.angleToGoal,relatePoint.translation.x(),relatePoint.translation.y()));
 		}
 	}
 
@@ -28,44 +35,48 @@ option(helpStriker)
 				//当strikerd的位置小于-50cm，supporter去左边
 			if(otherTeammate.pose.translation.y()<=-1000.f)
 					goto sideleft;
+			if(libCodeRelease.timeSinceBallWasSeen() > 10000)
+					goto searchForBall;
+			if(libCodeRelease.timeSinceBallWasSeen() < 300 && theRobotPose.translation.x()>2000.f)
+				goto turnToBall;
 		}
 		action{
+			theHeadControlMode = HeadControl::searchForBall;
 			Pose2f relatePoint = AbsolutePointToRobot(theRobotPose,3200.f,-1100.f);
 			WalkToTarget(Pose2f( setTurnVelocity,setAdjustVelocity,setAdjustVelocity),
-                    Pose2f(theBallModel.estimate.position.angle(),relatePoint.translation.x(),relatePoint.translation.y()));
+					Pose2f(libCodeRelease.angleToGoal,relatePoint.translation.x(),relatePoint.translation.y()));
 		}
 	}
-}
-option(Supporter)
-{
-  initial_state(start)
-  {
-    transition
-    {
-      if(state_time > 1000)
-        goto turnToBall;
-    }
-    action
-    {
-      theHeadControlMode = HeadControl::lookForward;
-      //Stand();
-    }
-  }
 
   state(turnToBall)
   {
     transition
     {
-      if(libCodeRelease.timeSinceBallWasSeen() > 7000)
+      if(libCodeRelease.timeSinceBallWasSeen() > 15000)
         goto searchForBall;
       if(std::abs(theBallModel.estimate.position.angle()) < 5_deg)
-        goto helpStriker;
+        goto stand;
     }
     action
     {
       theHeadControlMode = HeadControl::focusBall;
       WalkToTarget(Pose2f(50.f, 50.f, 50.f), Pose2f(theBallModel.estimate.position.angle(), 0.f, 0.f));
     }
+  }
+  state(stand)
+  {
+    transition
+    {
+      if(libCodeRelease.timeSinceBallWasSeen() < 300)
+        goto turnToBall;
+	 if(libCodeRelease.timeSinceBallWasSeen() > 15000)
+        goto searchForBall;
+    }
+    action
+    {
+      theHeadControlMode = HeadControl::focusBall;
+      Stand();
+  }
   }
   
  state(searchForBall)
@@ -74,7 +85,7 @@ option(Supporter)
     {
       if(libCodeRelease.timeSinceBallWasSeen() < 300)
         goto turnToBall;
-	  if(libCodeRelease.timeSinceBallWasSeen() > 17000)//若原地找球超过20秒(27000因为原先有7000的延时)，就回到中心
+	  if(libCodeRelease.timeSinceBallWasSeen() > 60000)//若原地找球超过20秒(27000因为原先有7000的延时)，就回到中心
 		  goto backToCenter;
     }
     action
@@ -86,23 +97,10 @@ option(Supporter)
   state(backToCenter){
 		  transition{
 			 if( libCodeRelease.timeSinceBallWasSeen() < 300  || (state_time > 10 && action_done) )
-					goto turnToBall;
+					goto start;
 			  }
 		 action{
 					ReadyState();
 			 }
 	  }
-  state(helpStriker)
-  {
-	  transition
-	  {
-		   if(libCodeRelease.timeSinceBallWasSeen() > 7000)
-				goto searchForBall; ;
-	  }
-	  action
-	  {
-		  theHeadControlMode = HeadControl::focusBall;
-		  helpStriker();
-	  }
-  }
 }
